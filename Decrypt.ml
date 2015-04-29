345257da4c57bc2c6bae7a6c83acc4329f2295e7
Oskar Kocol <kocol@college.harvard.edu>
2015-04-29 00:25:30 -0400

    Combination of lists is working

--- Decrypt.ml
+++ Decrypt.ml
@@ -4,16 +4,34 @@ open Core.Std
 let rotors = [1; 2; 3; 4; 5; 6; 7; 8]
 let total_rotors = 3
 
-(* run on utop and type in (rotor_possibilities total_rotors rotors)*)
-let rotor_possibilities (k: int) list = 
-    let rec aux (k) (acc) (emit) = function
-      | [] -> acc
-      | h :: t ->
-        if k = 1 then aux k (emit [h] acc) emit t else
-          let new_emit x = emit (h :: x) in
-          aux k (aux (k-1) acc new_emit t) emit t
-    in
-    let emit (x) (acc) = x :: acc in
-    aux k [] emit list;;
-;;
+let make_list k: ('a*'a*'a) : 'a list =
+  match k with
+  |(a, b, c) -> [a; b; c]
 
+(* run on utop and type in: create rotors rotors rotors*)
+let rec create (start: 'a list) (left: 'a list) (right: 'a list) : 'a list list=
+let rec list_of_two (x: 'a) (y: 'a) (l: 'a list) : 'a list list=
+  match l with
+  |[] -> []
+  |[hd] -> if hd = x || hd = y then []
+    else [[x; y; hd]]
+  |hd::tl -> if hd = x || hd = y then list_of_two x y tl
+    else [x; y; hd] :: (list_of_two x y tl) in
+  match left with
+  |[] -> []
+  |[hd1] ->
+    (match right with
+    |[] -> []
+    |[hd2] -> if hd1 = hd2 then []
+      else (list_of_two hd1 hd2 start) @ (create start [] [])
+    |hd2::tl2 -> if hd1 = hd2 then create start left tl2
+      else (list_of_two hd1 hd2 start) @ (create start left tl2)
+     )
+  |hd1::tl1 ->
+    (match right with
+    |[] -> []
+    |[hd2] -> if hd1 = hd2 then create start tl1 start
+      else (list_of_two hd1 hd2 start) @ (create start tl1 start)
+    |hd2::tl2 -> if hd1 = hd2 then create start left tl2
+      else (list_of_two hd1 hd2 start) @ (create start left tl2)
+    )
